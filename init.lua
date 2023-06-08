221a2ba815f55805ea5aa1e89d456ae784a223b1
German Odilov <german.odilov@outlook.com>
2023-06-08 16:00:12 +0600

    nd.nvim: init minor change

--- init.lua
+++ init.lua
@@ -15,9 +15,7 @@ init = function(root, is_debug)
         return
     end
 
-    if not root then
-        root = '.'
-    end
+    root = root or '.'
 
     local entry_root = path ~= '' and format('%s/%s', root, path) or root
 
--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 git submodule update --recursive --remote
 git add .
-git commit -m 'nd.res: submodule update'
+git commit -m 'nd.nvim: submodule update'
 git push origin main
